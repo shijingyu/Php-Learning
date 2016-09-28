1daf9eca271246475a74870e49e7e05b27830567
shijingyu <945090896@qq.com>
2016-09-28 21:36:58 +0800

    邮箱格式正则表达式

---
+++ 邮箱验证和邮箱编码格式.php
@@ -0,0 +1,18 @@
+//邮箱发送验证吗
+／／邮箱检验:
+<?php 
+
+
+if (strlen ($email) < 6){
+
+	echo 'That email address is not valid';
+
+}
+
+
+
+ ?>
+
+ //邮箱编码格式：
+
+^[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$
